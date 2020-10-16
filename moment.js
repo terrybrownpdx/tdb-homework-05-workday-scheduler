0c1559f20fc55f619142d820ddb6b9f4f1fb316e
tdb <tbrow69@msn.com>
2020-10-16 13:52:26 -0700

    created js page and initiated moment command.  updated html with divs

---
+++ moment.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+    var now = moment();
+
+    var calendarYearMonthDay = document.getElementById("currentDay")
+    var calendarHour = document.getElementById("hour")
+
+
+    moment().toString();
+    console.log(now);
+
+});
+   
+
+
+
