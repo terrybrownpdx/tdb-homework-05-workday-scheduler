e4dc43435efd1ba5ed593450c54db68a42a787a9
tdb <tbrow69@msn.com>
2020-10-16 16:38:41 -0700

    converted moment data into variables and consoled

--- moment.js
+++ moment.js
@@ -1,13 +1,32 @@
 $(document).ready(function () {
 
-    var now = moment();
-
-    var calendarYearMonthDay = document.getElementById("currentDay")
-    var calendarHour = document.getElementById("hour")
-
-
-    moment().toString();
+    var now = moment().toString("dddd, MMMM Do YYYY, h:mm:ss a");
     console.log(now);
+
+    var now1 = now.split(' ');
+    console.log(now1);  
+
+    //var today = [now1]
+    //console.log(today);
+
+    var day = now1 [0];
+    console.log(day);
+
+    var month = now1 [1];
+    console.log(month);
+
+    var date = now1 [2];
+    console.log(date);
+
+    var year = now1 [3];
+    console.log(year);
+
+    var hour = now1 [4];
+    console.log(hour);
+
+    //var calendarYearMonthDay = document.getElementById("currentDay")
+    //var calendarHour = document.getElementById("hour")   
+
 
 });
    
